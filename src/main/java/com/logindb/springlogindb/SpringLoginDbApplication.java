f8cc74fdd822b53be2cc0bd3c303a5055efc23ec
Ryan Llewellyn <36107368+R-Llewellyn96@users.noreply.github.com>
2021-03-21 21:24:18 +0000

    Initial commit

---
+++ src/main/java/com/logindb/springlogindb/SpringLoginDbApplication.java
@@ -0,0 +1,13 @@
+package com.logindb.springlogindb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logindb/springlogindb/SpringLoginDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logindb.springlogindb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
